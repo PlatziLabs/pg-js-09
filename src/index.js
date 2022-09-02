6d7c7bac3fcb13886a78f94bc19ec1f9a9e4c759
Nicolas Molina Monroy <hi@nicobytes.com>
2022-09-02 17:40:46 -0400

    :construction: work
    g

--- src/index.js
+++ src/index.js
@@ -2,27 +2,22 @@ import "./styles.css";
 import { runCode } from "./exercise";
 
 (async () => {
-
-  let rta = '';
-
   document.getElementById("app").innerHTML = `
     <h1>Fetch Util</h1>
     <p><input id="input" value="https://api.escuelajs.co/api/v1/categories" type="text" /></p>
-    <p><button id="btn">Fetch</button></p>
+    <p><button type="button" id="btn">Fetch</button></p>
     <p>Response from <code>runCode</code>
-    <pre><code>${rta}</code></pre>
+    <pre><code id="code"></code></pre>
   `;
 
   document.getElementById("btn").addEventListener("click", async () => {
     const url = document.getElementById("input").value;
-    console.log(url);
-    rta = await runCode(url);
-    document.getElementById("app").innerHTML = `
-      <h1>Fetch Util</h1>
-      <p><input id="input" value="https://api.escuelajs.co/api/v1/categories" type="text" /></p>
-      <p><button id="btn">Fetch</button></p>
-      <p>Response from <code>runCode</code>
-      <pre><code>${rta}</code></pre>
-    `;
+    let rta = "";
+    try {
+      rta = await runCode(url);
+    } catch (error) {
+      rta = error;
+    }
+    document.getElementById("code").innerHTML = rta;
   });
 })();
